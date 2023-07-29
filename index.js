fd25378c2980b00defda17fe24a379f3fd560d65
Chirag Maski <78970626+chiraghm4@users.noreply.github.com>
2023-07-30 02:57:25 +0530

    about route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({"hi": "there"})
-})
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.get("/about", (req, res) => {
+  res.send({ name: "Mojo Jojo", age: 21 });
+});
 
 app.listen(5000, () => {
-    console.log("connected to server")
-})
+  console.log("connected to server");
+});
